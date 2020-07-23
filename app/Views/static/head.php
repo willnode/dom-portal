cafd769645b37fa3261dca8288b62bba3a214e31
Wildan M <willnode@wellosoft.net>
2020-07-23 21:27:42 +0700

    minor css

--- app/Views/static/head.php
+++ app/Views/static/head.php
@@ -4,7 +4,6 @@
 	<link rel="shortcut icon" href="/logo.svg" />
 	<meta name="description" content="The small framework with powerful features">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/png" href="/favicon.ico" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<?php if (ENVIRONMENT === 'production') : ?>
 		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
