61ab0569818bd7c8b5e32ed024dac9722c2b28dd
Wildan M <willnode@wellosoft.net>
2020-09-24 12:54:40 +0700

    Host statuses

--- app/Commands/CronJob.php
+++ app/Commands/CronJob.php
@@ -12,6 +12,7 @@ use App\Models\HostStatModel;
 use App\Models\ServerModel;
 use App\Models\ServerStatModel;
 use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
 
 require_once APPPATH . 'Libraries' . DIRECTORY_SEPARATOR . 'spyc.php';
 
@@ -39,66 +40,74 @@ class CronJob extends BaseCommand
             /** @var Host[] */
             $hosts = (new HostModel())->atServer($server->id)->find();
             foreach ($hosts as $host) {
-                if ($domain = ($domains[$host->domain] ?? '')) {
-                    $stat = $host->stat;
-                    $plan = $host->plan;
-                    $newStat = [
-                        'host_id' => $host->id,
-                        'domain' => $host->domain,
-                        'identifier' => $domain['ID'],
-                        'password' => $domain['Password'],
-                        'quota_server' => intval($domain['Server byte quota used']),
-                        'quota_user' => intval($domain['User byte quota used']),
-                        'quota_db' => intval($domain['Databases byte size'] ?? 0),
-                        'quota_net' => intval($domain['Bandwidth byte usage'] ?? 0),
-                        'features' => $domain['Features'],
-                        'bandwidths' => json_encode($bandwidths[$host->domain] ?? null),
-                        'disabled' => $domain['Disabled'] ?? null,
-                        'updated_at' => date('Y-m-d H:i:s'),
-                    ];
-                    if (!$stat) {
-                        $stat = new HostStat($newStat);
-                    } else {
-                        if ($stat->quota_net > $newStat['quota_net']) {
-                            // Roll over time
-                            log_message('notice', 'ROLLOVER ' . $newStat['domain'] . ': ' . json_encode([$stat->quota_net, $newStat['quota_net']]));
-                            $host->addons = max(0, $host->addons - (($newStat['quota_net'] / 1024 / 1024) - ($plan->net * 1024 / 12)));
-                            (new VirtualMinShell())->adjustBandwidthHosting(
-                                ($host->addons + ($plan->net * 1024 / 12)),
-                                $host->domain,
-                                $server->alias
-                            );
-                            (new HostModel())->save($host);
-                        }
-                        $stat->fill($newStat);
+                if (!($domain = ($domains[$host->domain] ?? '')))
+                    continue;
+                $stat = $host->stat;
+                $plan = $host->plan;
+                $newStat = [
+                    'host_id' => $host->id,
+                    'domain' => $host->domain,
+                    'identifier' => $domain['ID'],
+                    'password' => $domain['Password'],
+                    'quota_server' => intval($domain['Server byte quota used']),
+                    'quota_user' => intval($domain['User byte quota used']),
+                    'quota_db' => intval($domain['Databases byte size'] ?? 0),
+                    'quota_net' => intval($domain['Bandwidth byte usage'] ?? 0),
+                    'features' => $domain['Features'],
+                    'bandwidths' => json_encode($bandwidths[$host->domain] ?? null),
+                    'disabled' => $domain['Disabled'] ?? null,
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ];
+                if (!$stat) {
+                    $stat = new HostStat($newStat);
+                } else {
+                    if ($stat->quota_net > $newStat['quota_net']) {
+                        // Roll over time
+                        log_message('notice', 'ROLLOVER ' . $newStat['domain'] . ': ' . json_encode([$stat->quota_net, $newStat['quota_net']]));
+                        $host->addons = max(0, $host->addons - (($newStat['quota_net'] / 1024 / 1024) - ($plan->net * 1024 / 12)));
+                        (new VirtualMinShell())->adjustBandwidthHosting(
+                            ($host->addons + ($plan->net * 1024 / 12)),
+                            $host->domain,
+                            $server->alias
+                        );
                     }
-                    (new HostStatModel())->replace($stat->toRawArray());
-                    $expired = time() >= $host->expiry_at->getTimestamp();
-                    $overDisk = ($stat->quota_server) > $plan->disk * 1024 * 1024;
-                    $overBw = ($stat->quota_net) > $plan->net * 1024 * 1024 * 1024 / 12 + $host->addons * 1024 * 1024;
-                    if (!$stat->disabled) {
-                        if ($overDisk) {
-                            // Disable
-                            (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'Running out Disk Space');
-                        } else if ($overBw) {
-                            // Disable
-                            (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'Running out Bandwidth');
-                        } else if ($expired) {
-                            // Disable
-                            (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'host expired');
-                        }
-                    } else {
-                        if (strtotime('-2 weeks', time()) >= $host->expiry_at->getTimestamp()) {
-                            if ($host->plan_id === 1) {
-                                // Paid hosts should be immune from this, in case error logic happens...
-                                (new VirtualMinShell())->deleteHosting($host->domain, $server->alias);
-                            }
-                            // TODO: Deleted email
-                        } else if (!($expired || $overDisk || $overBw)) {
-                            // Enable
-                            (new VirtualMinShell())->enableHosting($host->domain, $server->alias);
-                        }
+                    $stat->fill($newStat);
+                }
+                (new HostStatModel())->replace($stat->toRawArray());
+                $expired = time() >= $host->expiry_at->getTimestamp();
+                $overDisk = ($stat->quota_server) > $plan->disk * 1024 * 1024;
+                $overBw = ($stat->quota_net) > $plan->net * 1024 * 1024 * 1024 / 12 + $host->addons * 1024 * 1024;
+                CLI::write('INJURY TIME ' . json_encode([$host->domain, $stat->disabled, $expired, $overDisk, $overBw]));
+                if (!$stat->disabled) {
+                    if ($overDisk) {
+                        // Disable
+                        (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'Running out Disk Space');
+                        $host->status = 'suspended';
+                    } else if ($overBw) {
+                        // Disable
+                        (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'Running out Bandwidth');
+                        $host->status = 'suspended';
+                    } else if ($expired) {
+                        // Disable
+                        (new VirtualMinShell())->disableHosting($host->domain, $server->alias, 'host expired');
+                        $host->status = 'expired';
                     }
+                } else {
+                    if ((strtotime('-2 weeks', time()) >= $host->expiry_at->getTimestamp()) || ($stat->quota_server > $plan->disk * 1024 * 1024 * 3)) {
+                        if ($host->plan_id === 1) {
+                            // Paid hosts should be immune from this, in case error logic happens...
+                            (new VirtualMinShell())->deleteHosting($host->domain, $server->alias);
+                            $host->status = 'removed';
+                        }
+                        // TODO: Deleted email
+                    } else if (!($expired || $overDisk || $overBw)) {
+                        // Enable
+                        (new VirtualMinShell())->enableHosting($host->domain, $server->alias);
+                        $host->status = 'active';
+                    }
+                }
+                if ($host->hasChanged()) {
+                    (new HostModel())->save($host);
                 }
             }
             $data = [
